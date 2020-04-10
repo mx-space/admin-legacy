469b0a7555c56f492695b0e24b53541e1fea63c7
Innei <tukon479@gmail.com>
2020-04-10 14:56:14 +0800

    chore: remove vuetify

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,12 @@ import { PerfectScrollbar } from 'vue2-perfect-scrollbar'
 Vue.component('ps', PerfectScrollbar as VueConstructor<Vue>)
 
 import './plugins/font-awesome'
-import './plugins/vuetify'
 import './plugins/element-ui'
 import './plugins/processbar'
 import './plugins/kico-message'
 import { rest } from '@/api/rest'
 import 'vue-tsx-support/enable-check'
+
 Vue.prototype.$api = rest
 
 Vue.config.productionTip = false
