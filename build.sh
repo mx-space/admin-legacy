fe3567ddad0f40bae212461a289e359767194bed
Innei <tukon479@gmail.com>
2020-11-15 18:38:22 +0800

    chore: update build script

--- build.sh
+++ build.sh
@@ -3,14 +3,11 @@ cd
 cd admin
 rm -rf ./dist
 git pull
-yarn 
+yarn
 yarn build
 cd dist
 git init
 git add .
 git commit -m "Update: $(date)"
 git remote add origin git@github.com:Innei/admin-cdn.git
-git push  -u origin master -f
-cd 
-cd server
-cp ../admin/dist/index.html ./admin/index.html
+git push -u origin master -f
